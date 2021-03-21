2896db0184f5b1060b113d34f37ed95e82a4a7d9
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2021-03-21 17:40:36 +0600

    ok

--- Screens/ReviewForm/ReviewForm.js
+++ Screens/ReviewForm/ReviewForm.js
@@ -1,14 +1,24 @@
 import { Formik } from 'formik';
 import React from 'react';
-import { Button, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { globalStyles } from '../../Styles/Global';
+import * as yup from 'yup';
+
+const reviewSchema = yup.object({
+    title: yup.string().required().min(4),
+    body: yup.string().required().min(8),
+    rating: yup.string().required().test('is-num-1-5', 'Rating must be a number 1-5', (val) => {
+        return parseInt(val) < 6 && parseInt(val) > 0;
+    })
+})
 
 const ReviewForm = () => {
     return (
         <View style={globalStyles.container}>
             <Formik
             initialValue={{title: '', body: '', rating: ''}}
+            validationSchema={reviewSchema}
             onSubmit={(values, actions) => {
                 actions.resetForm();
                addReview(values); 
@@ -21,7 +31,9 @@ const ReviewForm = () => {
                       placeholder='Review title'
                       onChangeText={props.handleChange('title')}
                       value={props.values.title}
+                      onBlur={props.handleBlur('title')}
                       />
+                      <Text style={globalStyles.errorText}>{props.touched.title && props.errors.title}</Text>
 
                      <TextInput
                      multiline
@@ -29,7 +41,9 @@ const ReviewForm = () => {
                       placeholder='Review body'
                       onChangeText={props.handleChange('body')}
                       value={props.values.body}
+                      onBlur={props.handleBlur('body')}
                       />
+                       <Text style={globalStyles.errorText}>{props.touched.body && props.errors.body}</Text>
 
                     <TextInput
                      multiline
@@ -38,7 +52,9 @@ const ReviewForm = () => {
                       onChangeText={props.handleChange('rating')}
                       value={props.values.rating}
                       keyboardType="numeric"
+                      onBlur={props.handleBlur('rating')}
                       />
+                       <Text style={globalStyles.errorText}>{props.touched.rating && props.errors.rating}</Text>
 
                       <Button title='Submit' color='maroon' onPress={props.handleSubmit}/>
                   </View>
